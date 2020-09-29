3b3c5a2af403fbdf12abb80a52cfa472ed2d150d
AshishCh11 <72016621+AshishCh11@users.noreply.github.com>
2020-09-29 14:41:37 +0530

    Add files via upload

---
+++ company.ts
@@ -0,0 +1,19 @@
+
+import { Candidate } from './candidate';
+
+
+export interface Company{
+
+    id:number;
+    companyName:String;
+   //positions1 : Positions1[];
+   positionName :String;
+   Name:String;
+   Mobile:String;
+   Experience:String;
+   CurrentCTC:String;
+   ExpectedCTC:String;
+   NoticePeriod:String;
+   Remark:String;
+ 
+}
---
+++ positions.ts
@@ -0,0 +1,16 @@
+
+export class Positions {
+
+  
+    positionId:number;
+    positionName:String;
+    //candidate : Candidate[];
+    Name:String;
+   Mobile:String;
+   Experience:String;
+   CurrentCTC:String;
+   ExpectedCTC:String;
+   NoticePeriod:String;
+   Remark:String;
+    positions1:''
+}
